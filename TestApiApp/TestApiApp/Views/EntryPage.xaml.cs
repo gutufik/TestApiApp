eb3d0b7eb989a94f1d0a4fd6e4f4123abba24cab
Михаил Мясников <gutufik@mail.ru>
2022-04-25 12:19:48 +0300

    added on click open

--- TestApiApp/TestApiApp/Views/EntryPage.xaml.cs
+++ TestApiApp/TestApiApp/Views/EntryPage.xaml.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TestApiApp.Models;
+using Xamarin.Essentials;
 
 namespace TestApiApp.Views
 {
@@ -19,6 +20,12 @@ namespace TestApiApp.Views
             EntryModel = entry;
             InitializeComponent();
             this.BindingContext = EntryModel;
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var url = (sender as Label).Text;
+            Launcher.OpenAsync(new Uri(url));
         }
     }
 }
